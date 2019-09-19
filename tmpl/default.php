10666947bd4b8d2bd5943c1f0f9fb6f5b9a0c279
Olivier Buisard <olivier.buisard@simplifyyourweb.com>
2019-09-19 14:01:30 -0400

    v1.2.0
    bug fixes
    button template
    language additions
    images additions
    better redirects
    easier to read javascript code

---
+++ tmpl/default.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @plugin Paystack Payment Plugin for the Pay per Download component
+ * @author Adedayo Adeniyi
+ * @copyright (C) Adedayo Adeniyi
+ * @copyright (C) 2011 Simplify Your Web, Inc. All rights reserved
+ * @license GNU/GPL http://www.gnu.org/copyleft/gpl.html
+ */
+
+// no direct access
+defined('_JEXEC') or die;
+
+?>
+<button id="paystack-pay-btn" type="button">
+	<?php echo JText::_('PLG_PAYPERDOWNLOADPLUS_PAYSTACKPAY_PAYWITHPAYSTACK'); ?>
+</button>
